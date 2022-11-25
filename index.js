7fea457453a1c404a273961f0d04dea7a45f6da9
Benzene <oluuwolevictor70@gmail.com>
2022-11-25 22:35:02 +0100

    Add files via upload
    initial rabbit syndicate file

---
+++ index.js
@@ -0,0 +1,25 @@
+
+window.sr = ScrollReveal();
+sr.reveal(".navbar", {
+    duration: 2000,
+    origin: "bottom"
+});
+sr.reveal(".heading", {
+    duration: 2000,
+    origin: "left",
+    distance: "100px"
+});
+sr.reveal(".z", {
+    duration: 2000,
+    origin: "top",
+    distance: "100px",
+    viewFactor: 0.3,
+    delay: "250",
+});
+sr.reveal(".p", {
+    duration: 2000,
+    origin: "bottom",
+    distance: "100px",
+    delay: "250",
+    viewFactor: 0.3
+});
